d85ea59777835d109a6e4aeea4da78d9a105b53e
DpiTStudio <admin@dpitstudio.ru>
2025-04-16 00:04:13 +0300

    On branch w-1-2 Changes to be committed: 	modified:   mysite/manage.py 	modified:   mysite/mysite/__pycache__/settings.cpython-311.pyc 	modified:   mysite/newsapp/__pycache__/admin.cpython-311.pyc 	modified:   mysite/newsapp/__pycache__/models.cpython-311.pyc 	new file:   mysite/newsapp/__pycache__/views.cpython-311.pyc 	renamed:    mysite/newsapp/templates/news_list.html -> mysite/newsapp/templates/newsapp/news_list.html 	renamed:    mysite/newsapp/templates/news_snippet.html -> mysite/newsapp/templates/newsapp/news_snippet.html 	modified:   mysite/newsapp/views.py 	modified:   mysite/pages/__pycache__/urls.cpython-311.pyc 	modified:   mysite/pages/templates/base.html 	modified:   mysite/pages/urls.py

--- mysite/newsapp/views.py
+++ mysite/newsapp/views.py
@@ -1,12 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import News
-from news.models import News
-
-
-# def some_view(request):
-#     news = News.objects.filter(is_published=True)[:3]
-#     return render(request, "your_template.html", {"news": news})
 
 
 def news_list(request):
--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+# from news import views as news_views
+
 
 urlpatterns = [
+    # path("news/", news_views.news_list, name="news_list"),
     path("", views.home, name="home"),
     path("<slug:slug>/", views.page_detail, name="page_detail"),
 ]
