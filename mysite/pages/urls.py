5e7d1b9f5d43d3dc74851158a720a3858652137e
DpiTStudio <admin@dpitstudio.ru>
2025-04-15 14:22:28 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   mysite/pages/urls.py

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    # path("", views.home, name="home"),
     path("<slug:slug>/", views.page_detail, name="page_detail"),
 ]
